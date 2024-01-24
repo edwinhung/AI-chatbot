eee9cb7c1aa9a5ecbf17e68f2a9eb6e865ffaceb
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-01-24 00:12:24 -0800

    add openai api to backend

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { getAllUser, userLogin, userSignup } from '../controllers/user-controllers.js';
+import { getAllUser, userLogin, userSignup, verifyUser } from '../controllers/user-controllers.js';
 import { loginValidator, signupValidator, validate } from '../utils/validators.js'
+import { verifyToken } from '../utils/token-manager.js';
 
 const userRoutes = Router();
 
 userRoutes.get("/",getAllUser)
 userRoutes.post("/signup",validate(signupValidator),userSignup);
 userRoutes.post("/login",validate(loginValidator),userLogin);
+userRoutes.get("/auth-status", verifyToken, verifyUser);
 
 export default userRoutes;
